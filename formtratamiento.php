46b64f6e242a95903e3b632aee01df46db156ba3
manuh <hemmanuel44@gmail.com>
2015-10-23 15:17:53 -0300

    creado formulario para ingresar tratamiento

---
+++ formtratamiento.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ingreso de Tratamientos</title>
+</head>
+<body>
+	<form method="post" action="tratamientoabd.php">
+		Ingrese nombre del tratamiento:<br>
+		<input type="text" name="descripcion"><br>
+		Ingrese descripcion:<br>
+		<input type="text" name="obs"><br>
+		<input type="submit" value="Enviar">
+	</form>
+</body>
+</html>
