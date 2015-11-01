84ef18e535599783acd481e0d0e0029c5ec08fa2
manuh <hemmanuel44@gmail.com>
2015-11-01 17:14:30 -0300

    agregadas funciones para la agenda

--- functions.php
+++ functions.php
@@ -32,6 +32,78 @@ function getbddata () {
 	$conn->close();
 }
 
+function getdia ($i) {
+	$dia = "";
+	switch ($i) {
+		
+		case 1:
+			$dia = "lunes";
+			break;
+		case 2:
+			$dia = "martes";
+			break;
+		case 3:
+			$dia = "miercoles";
+			break;
+		case 4:
+			$dia = "jueves";
+			break;
+		case 5:
+			$dia = "viernes";
+			break;
+		default:
+			$dia = "lunes";
+			break;
+	}
+	return $dia;
+}
+
+function getmes ($i) {
+	$mes = "";
+	switch ($i) {
+		case 1:
+			$mes = "enero";
+			break;
+		case 2:
+			$mes = "febrero";
+			break;
+		case 3:
+			$mes = "marzo";
+			break;
+		case 4:
+			$mes = "abril";
+			break;
+		case 5:
+			$mes = "mayo";
+			break;
+		case 6:
+			$mes = "junio";
+			break;
+		case 7:
+			$mes = "julio";
+			break;
+		case 8:
+			$mes = "agosto";
+			break;
+		case 9:
+			$mes = "septiembre";
+			break;
+		case 10:
+			$mes = "octubre";
+			break;
+		case 11:
+			$mes = "noviemre";
+			break;
+		case 12:
+			$mes = "diciembre";
+			break;
+		default:
+			$mes = "Ingrese mes";
+			break;
+	}
+	return $mes;
+}
+?>
 <<<<<<< HEAD
 //filtrar dias ocupados
 function filtrarocupados($criterio, $valorini, $falorfin) {
