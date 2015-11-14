97390dcbd17f2c77e183fbbc04cd79531e6fa7b4
manuh <hemmanuel44@gmail.com>
2015-11-14 15:47:56 -0300

    base de datos para stand alone

---
+++ Ficha_dent.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 14, 2015 at 03:46 PM
+-- Server version: 5.5.46-0ubuntu0.14.04.2
+-- PHP Version: 5.5.9-1ubuntu4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `Ficha_dent`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Cliente`
+--
+
+CREATE TABLE IF NOT EXISTS `Cliente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(40) NOT NULL,
+  `apellido` varchar(40) NOT NULL,
+  `sexo` varchar(10) NOT NULL,
+  `mail` varchar(40) DEFAULT NULL,
+  `dir` varchar(40) NOT NULL,
+  `edad` int(2) NOT NULL,
+  `tel` int(8) DEFAULT NULL,
+  `ci` int(8) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Sesion`
+--
+
+CREATE TABLE IF NOT EXISTS `Sesion` (
+  `id` int(11) NOT NULL,
+  `id_cli` int(11) NOT NULL,
+  `id_trat` int(11) NOT NULL,
+  `fecha` int(11) NOT NULL,
+  `dia` int(11) NOT NULL,
+  `mes` int(11) NOT NULL,
+  `precio` int(11) DEFAULT NULL,
+  `descuento` int(11) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Tratamiento`
+--
+
+CREATE TABLE IF NOT EXISTS `Tratamiento` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(40) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
