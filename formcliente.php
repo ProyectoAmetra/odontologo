ca57d478d333da1bb60f1f808b4486787cec3be1
manuh <hemmanuel44@gmail.com>
2015-11-22 12:20:35 -0300

    agregado formulario y procesador para cliente

---
+++ formcliente.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ingreso de Tratamientos</title>
+</head>
+<body>
+	<form method="post" action="pacienteabd.php">
+		Nuevo Cliente:<br>
+		Nombre:
+		<input type="text" name="nombre"><br>
+		Apellido:
+		<input type="text" name="apellido"><br>
+		Sexo:
+		<input type="text" name="apellido"><br>
+		Mail:
+		<input type="text" name="mail"><br>
+		Direccion:
+		<input type="text" name="dir"><br>
+		Edad:
+		<input type="text" name="edad"><br>
+		Tel./Cel.:
+		<input type="text" name="tel"><br>
+		C.I.:
+		<input type="text" name="ci"><br>
+		<input type="submit" value="Enviar">
+	</form>
+</body>
+</html>
